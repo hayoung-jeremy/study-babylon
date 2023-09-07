1be5b4b477d4c9f98377bddf07d7a66cabc6542f
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-09-07 14:46:23 +0900

    feat : add lights and UI

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,11 @@
 import "./style.css";
 import * as BABYLON from "@babylonjs/core";
-import "@babylonjs/loaders/glTF/2.0";
 
 const canvas = document.getElementById("rendererCanvas") as HTMLCanvasElement;
 const engine = new BABYLON.Engine(canvas, true);
 const scene = new BABYLON.Scene(engine);
-const camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 4, new BABYLON.Vector3(0, 0, 0), scene);
+const camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 15, new BABYLON.Vector3(0, 0, 0), scene);
 camera.attachControl(canvas, true);
-
-scene.createDefaultEnvironment({
-  environmentTexture: "/envs/forest.env",
-  skyboxTexture: "/envs/forest.env",
-});
-
-BABYLON.SceneLoader.ImportMeshAsync("", "/models/DamagedHelmet.glb", "", scene).then(helmet => {
-  helmet.meshes[0].position.y = 2;
-  camera.setTarget(helmet.meshes[0]);
-
-  scene.onBeforeRenderObservable.add(() => {
-    helmet.meshes[0].rotate(new BABYLON.Vector3(0, 1, 0), 0.001);
-  });
-});
 
 engine.runRenderLoop(() => {
   scene.render();
@@ -29,3 +14,28 @@ engine.runRenderLoop(() => {
 window.addEventListener("resize", () => {
   engine.resize();
 });
+
+const primitives = [];
+
+for (let i = 0; i < 20; i++) {
+  let sphere = BABYLON.MeshBuilder.CreateSphere("", {}, scene);
+  sphere.position = new BABYLON.Vector3(Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10);
+  primitives.push(sphere);
+}
+
+const directionalLight = new BABYLON.DirectionalLight("directionalLight", new BABYLON.Vector3(0, -1, 0), scene);
+const pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0, 0, 0), scene);
+const hemisphereLight = new BABYLON.HemisphericLight("hemisphericLight", new BABYLON.Vector3(0, 1, 0), scene);
+const spotLight = new BABYLON.SpotLight(
+  "spotLight",
+  new BABYLON.Vector3(0, 0, 0),
+  new BABYLON.Vector3(0, 1, 0),
+  Math.PI / 4,
+  1,
+  scene
+);
+
+directionalLight.diffuse = new BABYLON.Color3(1, 0, 0);
+pointLight.diffuse = new BABYLON.Color3(0, 0, 1);
+hemisphereLight.diffuse = new BABYLON.Color3(1, 1, 0);
+spotLight.diffuse = new BABYLON.Color3(0, 1, 0);
