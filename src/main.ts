24f84430ae7f00964228e1fbd8240eb41caede7d
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-09-07 10:25:46 +0900

    feat : feat : Reflective 3D model with Engine, Scene, and Camera

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders/glTF/2.0";
 
 const canvas = document.getElementById("rendererCanvas") as HTMLCanvasElement;
 const engine = new BABYLON.Engine(canvas, true);
@@ -7,7 +8,19 @@ const scene = new BABYLON.Scene(engine);
 const camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 4, new BABYLON.Vector3(0, 0, 0), scene);
 camera.attachControl(canvas, true);
 
-const box = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
+scene.createDefaultEnvironment({
+  environmentTexture: "/envs/forest.env",
+  skyboxTexture: "/envs/forest.env",
+});
+
+BABYLON.SceneLoader.ImportMeshAsync("", "/models/DamagedHelmet.glb", "", scene).then(helmet => {
+  helmet.meshes[0].position.y = 2;
+  camera.setTarget(helmet.meshes[0]);
+
+  scene.onBeforeRenderObservable.add(() => {
+    helmet.meshes[0].rotate(new BABYLON.Vector3(0, 1, 0), 0.001);
+  });
+});
 
 engine.runRenderLoop(() => {
   scene.render();
