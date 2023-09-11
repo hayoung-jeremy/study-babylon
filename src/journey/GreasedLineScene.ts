5ced0771eb0080a5654315c527846b885c0c4e13
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-09-11 13:58:39 +0900

    feat : 텍스트 그리고, 폰트 불러오는 함수

--- src/journey/GreasedLineScene.ts
+++ src/journey/GreasedLineScene.ts
@@ -1,6 +1,10 @@
 import {
+  CreateGreasedLine,
   CreateGround,
   CubeTexture,
+  GreasedLineMeshColorMode,
+  GreasedLineMeshMaterialType,
+  GreasedLinePoints,
   HemisphericLight,
   PBRMaterial,
   Vector3,
@@ -21,6 +25,29 @@ const initGreasedLineScene = () => {
   const envTexture = CubeTexture.CreateFromPrefilteredData("/envs/night.env", scene);
   scene.environmentTexture = envTexture;
   scene.createDefaultSkybox(scene.environmentTexture);
+
+  const drawText = (points: GreasedLinePoints) => {
+    const textLines = CreateGreasedLine(
+      "textLines",
+      {
+        points,
+      },
+      {
+        materialType: GreasedLineMeshMaterialType.MATERIAL_TYPE_PBR,
+        colorMode: GreasedLineMeshColorMode.COLOR_MODE_MULTIPLY,
+        width: 0.8,
+      }
+    );
+
+    return textLines;
+  };
+
+  const getFont = async () => {
+    const response = await fetch(
+      "https://res.cloudinary.com/edorblewebvr/raw/upload/v1688485231/Tilt_Neon_Regular_ubg5mu.json"
+    );
+    return response.json();
+  };
 };
 
 export default initGreasedLineScene;
